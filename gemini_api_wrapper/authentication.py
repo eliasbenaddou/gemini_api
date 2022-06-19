3cf995ae3a8987d7ac750d8353e122fedb9f465f
Elias Benaddou Idrissi <eliasbenaddouidrissi@Eliass-MBP.cust.communityfibre.co.uk>
2022-06-19 23:08:19 +0100

    First commit

---
+++ gemini_api_wrapper/authentication.py
@@ -0,0 +1,95 @@
+import base64
+import hashlib
+import hmac
+import json
+import time
+import yaml
+from datetime import datetime
+
+import requests
+
+GEMINI_SANDBOX_BASE_URL = "https://api.sandbox.gemini.com"
+GEMINI_REQUEST_BASE_URL = "https://api.gemini.com"
+
+
+class Authentication(object):
+    """
+    Class to authenticate authentication.
+
+    Class provides methods to authenticate and make requests to
+    Gemini's Private REST API.
+
+    Attributes:
+        _public_key: a public key for authentication
+        _private_key: a private_key for authentication
+        _url: base URL for Gemini API
+
+    Methods:
+        make_request: makes a request to an endpoint URL
+    """
+
+    __slots__ = ["_public_key", "_private_key", "_url"]
+
+    def __init__(
+        self, sandbox: bool = False
+    ) -> None:
+        """
+        Initialise authentication
+
+        Args:
+            sandbox: flag for connecting to Sandbox environment
+        """
+
+        with open('config_variables.yml', 'r') as f:
+            keys = yaml.safe_load(f)
+
+        self._public_key: str = keys.get('public_key')
+        self._private_key: str = keys.get('private_key')
+
+        if sandbox:
+            self._url = GEMINI_SANDBOX_BASE_URL
+        else:
+            self._url = GEMINI_REQUEST_BASE_URL
+
+    def make_request(self, endpoint: str, payload: dict = None) -> dict:
+        """
+        Makes a request to an endpoint in the API
+
+        Args:
+            endpoint: String to add to base URL
+            paylod: Data to pass into encoded payload
+
+        Returns:
+            Dictionary containing response data
+        """
+
+        if not payload:
+            payload = {}
+
+        request_url = self._url + endpoint
+
+        payload["request"] = endpoint
+        payload["nonce"] = str(
+            int(time.mktime(datetime.now().timetuple()) * 1000)
+        )
+
+        encoded_payload = json.dumps(payload).encode("utf-8")
+        b64 = base64.b64encode(encoded_payload)
+        signature = hmac.new(
+            self._private_key.encode("utf-8"), b64, hashlib.sha384
+        ).hexdigest()
+
+        request_headers = {
+            "Content-Type": "text/plain",
+            "Content-Length": "0",
+            "X-GEMINI-APIKEY": self._public_key,
+            "X-GEMINI-PAYLOAD": b64,
+            "X-GEMINI-SIGNATURE": signature,
+            "Cache-Control": "no-cache",
+        }
+
+        request = requests.post(
+            request_url, data=None, headers=request_headers
+        )
+        data = request.json()
+        return data
